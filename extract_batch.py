3bd2086ade91c5a543d1306bd25d52dbe7bb306b
baker3x <131420337+baker3x@users.noreply.github.com>
2025-03-30 16:59:59 -0400

    extract_batch.py

--- extract_batch.py
+++ extract_batch.py
@@ -6,10 +6,10 @@ import sys
 
 # --- Configuration ---
 # Set the path to the TOP-LEVEL FOLDER containing your PDF files and subfolders
-pdf_base_folder_path = '/Users/baker3x/UnpluggedAI/gemma/pdfs' # <--- CHANGE THIS (The folder holding the subfolders)
+pdf_base_folder_path = '/Users/baker3x/pdfs' # <--- CHANGE THIS (The folder holding the subfolders)
 
 # Set the path to the FOLDER where you want the mirrored output structure saved
-output_base_folder_path = '/Users/baker3x/UnpluggedAI/gemma/pdfs_done' # <--- CHANGE THIS
+output_base_folder_path = '/Users/baker3x/pdfs_done' # <--- CHANGE THIS
 # --- End Configuration ---
 
 def extract_text_from_pdf(pdf_path):
